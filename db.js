8fc768316538a91fb07b65e3aeb86af0e91ec024
Susikumar <susikumar0123@gmail.com>
2026-08-29 18:08:35 +0530

    Add location route

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+
 require('dotenv').config();
 
 const pool = new Pool({
@@ -17,7 +18,6 @@ pool.connect((err, client, release) => {
     }
 });
 
-// Helper function to convert MySQL '?' style queries to PostgreSQL '$1, $2' style
 const db = {
     query: (text, params, callback) => {
         let queryText = text;
@@ -28,15 +28,15 @@ const db = {
             queryParams = [];
         }
 
-        // Convert ? to $1, $2, etc.
         let counter = 1;
+
         queryText = queryText.replace(/\?/g, () => `$${counter++}`);
 
         return pool.query(queryText, queryParams, (err, res) => {
             if (err) {
                 return callback(err, null);
             }
-            // Return results in an array format compatible with mysql callback style (results, fields)
+
             callback(null, res.rows);
         });
     }
